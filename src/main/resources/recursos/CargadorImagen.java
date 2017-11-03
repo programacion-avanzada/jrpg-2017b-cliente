187cff735199eb3b6f9b5148d8ee4663fec722d4
Mariano Nicolás Molina <marianoo.molina@gmail.com>
2017-11-03 20:26:23 -0300

    Reduccion de incidencias gradle en recursos.CargdorImagen

--- src/main/resources/recursos/CargadorImagen.java
+++ src/main/resources/recursos/CargadorImagen.java
@@ -6,8 +6,18 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
+/**
+ * 	
+ * Clase que carga las imagenes de los recursos
+ *
+ */
 public class CargadorImagen {
 
+	/**
+	 * Constructor de la clase
+	 * @param path ruta de la imagen a cargar
+	 * @return	devuelve la imgaen cargada
+	 */
     public static BufferedImage cargarImagen(final String path) {
 	try {
 	    return ImageIO.read(CargadorImagen.class.getResource(path));
